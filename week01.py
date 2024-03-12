0014eaf4698356cc03b6c33abc17f051b59bc859
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2024-03-12 18:03:03 +0900

    2024.03.12

--- week01.py
+++ week01.py
@@ -1,1 +1,22 @@
-print("Hello, World")
+# print("Hello, World")
+
+# # Q1.3
+# print(100) # 100
+# print(100 + 200) # 300
+# print('100 + 200') # 100 + 200
+# print(100, 200) # 100 200
+# print('Hello', 'Python', '!') # Hello Python !
+# print('Hello' + 'Python' + '!') # HelloPython!
+
+# # Q1.6
+# print('*' * 20)
+# print('안녕하세요')
+# print('저는 홍길동입니다')
+# print('*' * 20)
+
+# Q1.9
+print(9 ** 3) # 9 ^ 3
+print(9 / 3) # 3.0
+print(9 / 2) # 4.5
+print(9 // 3) # 3
+print(9 // 2) # 4
