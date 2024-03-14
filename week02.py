0116485bd7569580950ff27874fa66647a8697f3
Greenviee <a01047463862@gmail.com>
2024-03-14 18:01:01 +0900

    2024.03.14
    수업내용

---
+++ week02.py
@@ -0,0 +1,53 @@
+"""
+반지름을 사용해서 면적과 둘레를 계산하는 코드 입니다.
+"""
+
+# radius = 5.0
+# print('원의 반지름', radius)
+# print('원의 둘레', 2.0 * radius * 3.14)
+
+# width = 10 # 변수의 선언과 할당
+# height = 5 # 변수의 선언과 할당
+# width = 20 # 변수의 선언과 할당
+# rectangle_area = width * height
+# print('사각형의 면적 :', rectangle_area) # 계산 결과는?
+
+# num = 85
+# print(type(num)) # int
+# num = 85 * '10'
+# print(type(num)) # str
+# pi = 3.14159
+# print(type(pi)) # float
+# message = "Good morning"
+# print(type(message)) # str
+
+# l = [100, 300, 500, 900]
+# print(type(l)) # list
+# d = {'apple': 3000,
+# 'banana': 4200}
+# print(type(d)) # dict
+# t = ('홍길동'
+# , 30,
+# '율도국의 왕')
+# print(type(t)) # tupㅣe
+
+"""
+1. 정적
+- 특정 시점에 계산이 완료
+- 검증 가능
+
+2. 동적 
+- 현재 시점에 계산하고 있는 중
+- 검증이 힘듦
+"""
+
+# txt6 = '''Let's go'''
+# txt7 = '''큰따옴표(")와 작은따옴표(')를 모두 포함한 문장'''
+# print(txt6)
+# print(txt7)
+
+# num1 = num2 = num3 = 200 # 다중 할당문
+# print(num1, num2, num3) # 200 200 200
+# num4, num5 = 300, 400 # 동시 할당문
+# print(num4, num5) # 300 400
+
