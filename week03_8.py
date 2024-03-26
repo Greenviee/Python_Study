1a8887da2fabf1ce466e3980b6e4d1a72f1a349e
Greenviee <a01047463862@gmail.com>
2024-03-26 15:43:11 +0900

    2024.03.26

--- week03_8.py
+++ week03_8.py
@@ -2,5 +2,11 @@ num = str(input("정수를 입력하세요 : "))
 length = len(num)
 isInverted = True
 
-for i in range(0, length / 2):
-    
+for i in range(0, length // 2):
+    if num[i] != num[length - i - 1]:
+        isInverted = False
+        break
+if isInverted:
+    print(f"{num}은 거꾸로 정수입니다.")
+else:
+    print(f"{num}은 거꾸로 정수가 아닙니다.")
